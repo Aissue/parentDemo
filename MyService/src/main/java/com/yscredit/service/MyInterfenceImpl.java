f63d3aa1da20c0586ff17a93b40889bf2e501768
mengtian <mengtian@yscredit.com>
2018-04-03 11:35:01 +0800

    实现提供者扫描暴露接口

--- MyService/src/main/java/com/yscredit/service/MyInterfenceImpl.java
+++ MyService/src/main/java/com/yscredit/service/MyInterfenceImpl.java
@@ -1,7 +1,9 @@
 package com.yscredit.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.yscredit.interfence.IMyInterfence;
 
+@Service
 public class MyInterfenceImpl implements IMyInterfence {
     public String helloWorld() {
         return "hello dubbo!";
